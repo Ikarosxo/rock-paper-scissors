4647ee9c6160bf48569d4dbc3b51d70e8cca682e
Ikarosxo <94879916+Ikarosxo@users.noreply.github.com>
2025-03-28 14:14:37 -0400

    function for computer choice created

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function getComputerChoice(){
+    let number = getRandomInt(3);
+    var choice;
+    switch (number){
+        case 0:
+            return choice = "Rock";
+            break;
+        case 1:
+            return choice = "Paper";
+            break;
+        case 2:
+            return choice = "Scissors";
+            break;
+        default:
+            console.log("I believe you've might've broken this somehow");
+    }
+}
+console.log(getComputerChoice());
